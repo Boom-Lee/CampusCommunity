ecb2e4fc2cebcf00bf9589a68ef31f0e5bffb9b8
Boom-Lee <2071404994@qq.com>
2021-05-18 09:51:43 +0800

    apk 用户关注列表问题

--- app/app/src/main/java/com/liyuji/app/utils/Util.java
+++ app/app/src/main/java/com/liyuji/app/utils/Util.java
@@ -8,8 +8,8 @@ public class Util {
     /**
      * 手机调试本机地址
      */
-//    public final static String SERVER_ADDR = "http://8.136.180.205:8181/api/";
-    public final static String SERVER_ADDR = "http://192.168.3.25:8181/api/";
+    public final static String SERVER_ADDR = "http://8.136.180.205:8181/api/";
+//    public final static String SERVER_ADDR = "http://192.168.3.25:8181/api/";
     public static final String NET_UNCONNECTION = "网络未连接";
     public static final int ARITCLEFRAGMENT = 1;
     public static final int SCHEDULERAGMENT = 2;
